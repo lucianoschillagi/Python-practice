6da737f879a39f63ba853c8dd171790578276d2f
luko <luko.web@gmail.com>
2018-11-02 15:29:12 -0300

    first commit

---
+++ python-syntax.py
@@ -0,0 +1,19 @@
+# Print Statemnts
+print 'Hello Python' # Python 2
+print('Hello Python') # Python 3
+
+# string
+print "Hello"
+# String Concatenation
+print "Hello " + "Luciano"
+
+# Variables
+todays_date = "2 Nov 2018"
+print todays_date
+
+# Arithemetics
+mirthful_addition = 12381 + 91817
+amazing_subtraction = 981 - 312
+trippy_multiplication = 38 * 902
+happy_division = 540 / 45
+sassy_combinations = 129 * 1345 + 120 / 6 - 12

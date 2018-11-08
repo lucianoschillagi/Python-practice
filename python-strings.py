adaae2ad87894c6776fe2c5f90afc932567d447b
luko <luko.web@gmail.com>
2018-11-08 19:52:00 -0300

    feat: Add python strings file

---
+++ python-strings.py
@@ -0,0 +1,10 @@
+# PYTHON Strings
+
+# Use the len method to print the length of the string.
+
+x = "Hello World"
+print(len(x))
+
+# Get the first character of the string txt.
+txt = "Hello World"
+x = txt[0]
--- string-methods.py
+++
@@ -1,7 +0,0 @@
-# WW3 Phyton String
-
-# Return the string without any whitespace at the beginning or the end.
-
-txt = " Hello World "
-txt.strip()
-
